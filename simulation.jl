e84ba35453275ddcac7979a2bca7cd03ceadca10
Luke Asperger <lukeasperger@Lukes-MacBook-Pro-2.local>
2018-11-14 14:23:17 -0800

    Intial commit simulations

---
+++ simulation.jl
@@ -0,0 +1,12 @@
+using Printf
+
+"""
+    simulate_round(state, action, opp_policies)
+
+    Takes in player's state and action as well as an array of opponent policies
+    and simulates game up until the player's next turn. Array of opponent
+    policies should be in turn order.
+"""
+function simulate_round(policies, starting_player)
+    
+end
